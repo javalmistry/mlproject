956bd2da1f63031913c35b6f0f568041739dd236
javal <javalmistry@hotmail.com>
2025-04-24 19:02:14 +0100

    sysnc

--- ahello.py
+++ ahello.py
@@ -1,7 +1,33 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
+    return 'Index Page 1'
+
+
+@app.route("/hello")
+def hello():
     return "<h1>This is index page</h1><p>Hello, World!</p>"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f' Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {esacpe(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The Project Page'
+
+@app.route('/about')
+def abount():
+    return 'The About Page'
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+#from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -26,7 +27,7 @@ if __name__ == "__main__":
         a = 1/0
 
     except Exception as e:
-        logging.info("Devide by Zero")
+        logging.info("Devide by Zero 1")
         raise CustomException(e,sys)
 
 
